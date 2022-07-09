eb84c43fe903d140ea3ceefbab5734725130c59c
Alona Chepelevych <alyona.chepelevich@gmail.com>
2022-07-09 16:50:31 +0300

    Added prototypes of functions

--- practice/practice.h
+++ practice/practice.h
@@ -4,7 +4,20 @@
 #define PUBLIC
 #define ERRORRANGE -1
 
+/**
+* Функція перевірки діапазону
+*
+* @param hour число, min число
+* @return (hour >= 0 && hour < 24) && (min >= 0 && min < 60) значення виразу
+*/
 PUBLIC int correct(int hour, int min);
+
+/**
+* Функція конвертації чисел у текст
+*
+* @param х число
+* @return ending число
+*/
 PUBLIC int convert(int x);
 
 #endif
