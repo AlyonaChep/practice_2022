0824fa0615c47143977383a845433cccca5b6f0e
Alona Chepelevych <alyona.chepelevich@gmail.com>
2022-07-09 16:56:43 +0300

    Added comments and removed unused header files

--- practice/main.c
+++ practice/main.c
@@ -1,15 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <locale.h>
 #include "practice.h"
 
 int main(void)
 {
 	setlocale(LC_CTYPE, "Ukr"); // зміна кодової таблиці символів
-	int hour, min; // година, хвилина
-	const char* ending_arr[] = { "", "а", "и" }; // закінчення для годин та хвилин
+	int hour, min; // змінні для годин, хвилин
+	const char* ending_arr[] = { "", "а", "и" }; // масив закінчень для годин та хвилин
 	// ввод 
 	do {
 		printf("Введiть корректний час: ");
@@ -17,7 +15,8 @@ int main(void)
 	} while (correct(hour, min) == ERRORRANGE); // перевірка діапазону
 	// вивод
 	printf("Введений час: ");
-	printf("годин%c ", *ending_arr[convert(hour)]);
-	printf("хвилин%c\n", *ending_arr[convert(min)]);
+	printf("годин%c ", *ending_arr[convert(hour)]);  // | виклик функції, її вивод 
+	printf("хвилин%c\n", *ending_arr[convert(min)]); // | та обрання потрібного закінчення
+	// завершення функції
 	return 0;
 }
