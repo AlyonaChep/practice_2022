6c7750debe4a5f2daa21cd0fde6075c11010bd10
Alona Chepelevych <alyona.chepelevich@gmail.com>
2022-07-11 00:48:34 +0300

    Replacing a string array with a char array

--- practice/main.c
+++ practice/main.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	setlocale(LC_CTYPE, "Ukr"); // зміна кодової таблиці символів
 	int hour, min; // змінні для годин, хвилин
-	const char* ending_arr[] = { "", "а", "и" }; // масив закінчень для годин та хвилин
+	const char ending_arr[] = { '\0', 'а', 'и' }; // масив закінчень для годин та хвилин
 	int ending = 0; // значення індексу
 	char hour_res[20] = ""; // масив результату перетворення годин
 	char min_res[20] = ""; // масив результату перетворення хвилин
@@ -20,9 +20,9 @@ int main(void)
 	printf("Введений час: ");
 	// виклик функції, її вивод та обрання потрібного закінчення
 	ending = convert(hour, hour_res);
-	printf("%s годин%c ", hour_res, *ending_arr[ending]);
+	printf("%s годин%c ", hour_res, ending_arr[ending]);
 	ending = convert(min, min_res);
-	printf("%s хвилин%c \n", min_res, *ending_arr[ending]);
+	printf("%s хвилин%c \n", min_res, ending_arr[ending]);
 	// завершення функції
 	return 0;
 }
